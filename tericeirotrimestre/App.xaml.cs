3641c4c39f872d6dccbcfe057b44027de7b3c4ae
julia <juliareisdasilva2006@gmail.com>
2024-09-17 11:49:56 -0300

    17/09

---
+++ tericeirotrimestre/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace tericeirotrimestre;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ tericeirotrimestre/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace tericeirotrimestre;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
